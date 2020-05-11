dcd9cd2ac76b8be38b62310a769e26e6317be008
Vishal Srinivas <vishal1995srinivas@gmail.com>
2020-05-11 14:24:01 +0530

    Join componenent

--- client/chat/src/Components/Join.js
+++ client/chat/src/Components/Join.js
@@ -1,7 +1,22 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 const Join = () => {
-	return <div>Join</div>;
+	const [ name, setName ] = useState('');
+	const [ room, setRoom ] = useState('');
+
+	return (
+		<div className="joinOuterContainer">
+			<div className="joinInnerContainer">
+				<h1 className="heading">Join</h1>
+			
+			<div><input placeholder="Name" className="joinInput" type="text" onChange={} /></div>
+			<div><input placeholder="Room" className="joinInput mt-20" type="submit" onChange={} /></div>
+			<Link>
+				<button className="button mt-20" type="submit">Sign-In</button>
+			</Link>
+		</div>
+		</div>
+	);
 };
 
 export default Join;
