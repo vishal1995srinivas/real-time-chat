c60263a2039b8e171bba96912368ae7d0fa964e4
Vishal Srinivas <vishal1995srinivas@gmail.com>
2020-05-12 09:49:05 +0530

    get user

--- users.js
+++ users.js
@@ -18,5 +18,7 @@ const removeUser = (id) => {
 		return users.splice(index, 1)[0];
 	}
 };
-const getUser = () => {};
+const getUser = (id) => {
+	users.find((user) => user.id === id);
+};
 const getUsersInRoom = () => {};
