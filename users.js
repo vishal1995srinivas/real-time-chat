a625af9de54017b4efa022d5e63c95963997a796
Vishal Srinivas <vishal1995srinivas@gmail.com>
2020-05-12 09:47:49 +0530

    remove user

--- users.js
+++ users.js
@@ -12,6 +12,11 @@ const addUser = () => {
 
 	return { user };
 };
-const removeUser = () => {};
+const removeUser = (id) => {
+	const index = users.findIndex((user) => user.id === id);
+	if (index !== -1) {
+		return users.splice(index, 1)[0];
+	}
+};
 const getUser = () => {};
 const getUsersInRoom = () => {};
