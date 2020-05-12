de65b80bcbb9b43cf2238dcc0babad461950f279
Vishal Srinivas <vishal1995srinivas@gmail.com>
2020-05-12 09:56:45 +0530

    get user in rooms

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
-
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 const PORT = process.env.PORT || 5000;
 const router = require('./router');
 
